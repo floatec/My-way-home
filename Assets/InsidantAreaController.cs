42887067e69a17e812185e41ebf4a08dd68d6871
Benkei <Kutsarit@web.de>
2015-01-24 14:44:52 +0100

    bugfix

--- Assets/InsidantAreaController.cs
+++ Assets/InsidantAreaController.cs
@@ -1,47 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InsidantAreaController : MonoBehaviour {
+public class InsidantAreaController : MonoBehaviour
+{
 	private bool triggered = false;
 	public VictomController[] Victims;
 	public EnemyController[] Enemies;
 	void OnTriggerEnter ( Collider other )
 	{
-		if (  other.CompareTag ( "Player" ) )
+		if ( other.CompareTag ( "Player" ) )
 		{
-			if(!triggered){
+			if ( !triggered )
+			{
 				Debug.Log ( "fight !!!" );
-				triggered=true;
-				foreach(var enemy in Enemies){
-					enemy.isActive=true;
+				triggered = true;
+				foreach ( var enemy in Enemies )
+				{
+					enemy.isActive = true;
 				}
 			}
-			other.gameObject.GetComponent<Player>().iac=this;
+			other.gameObject.GetComponent<Player> ().iac = this;
 		}
 
 	}
 	void OnTriggerExit ( Collider other )
 	{
-		if (  other.CompareTag ( "Player" ) )
+		if ( other.CompareTag ( "Player" ) )
 		{
-			other.gameObject.GetComponent<Player>().iac=null;
+			other.gameObject.GetComponent<Player> ().iac = null;
 		}
-		
+
 	}
 
 
-	void Update(){
-		if (triggered) {
+	void Update ()
+	{
+		if ( triggered )
+		{
 
 		}
 	}
 
-	public bool isStronger(float strongnes){
+	public bool isStronger ( float strongnes )
+	{
 		float count = 0;
-		foreach(var enemy in Enemies){
-			count+= enemy.dangourLevel;
+		foreach ( var enemy in Enemies )
+		{
+			count += enemy.dangourLevel;
 		}
 		return strongnes >= count;
 	}
-	
+
 }
