fd6923f9bee28192e91d4d9dd60e9e94880e3743
floatec <mp@floatec.de>
2015-01-25 02:50:33 +0100

    glow baby glow!!!!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,32 +11,33 @@ public class Player : MonoBehaviour
 	public float karma;
 	public WatcherController[] watchers;
 	public WorldController world;
-
+	public GameObject glowIndicator;
+	
 	public float helpCallRange = 10;
-
+	
 	public GameObject SpeechbubblePrefab;
 	public GameObject PolicePrefab;
-
+	
 	private float strongnes = 1;
 	private Vector3 moveTarget;
 	private NavMeshAgent agent;
-
+	
 	void Start ()
 	{
 		agent = GetComponent<NavMeshAgent> ();
 		moveTarget = transform.position;
 	}
-
+	
 	void Update ()
 	{
 		bool mouseOverUI = EventSystem.current != null && EventSystem.current.currentSelectedGameObject != null;
-
+		
 		if ( mouseOverUI )
 			return;
-
+		
 		var ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
 		RaycastHit hit;
-
+		
 		if ( Input.GetMouseButton ( 0 ) && Physics.Raycast ( ray, out hit, 1000, terrainLayer ) )
 		{
 			if ( 1 << hit.collider.gameObject.layer == terrainLayer )
@@ -44,7 +45,7 @@ public class Player : MonoBehaviour
 				moveTarget = hit.point;
 			}
 		}
-		if ( iac!=null && Input.GetMouseButtonUp ( 0 ) && Physics.Raycast ( ray, out hit, 1000, interUnitLayer | WatcherLayer ) )
+		if ( Input.GetMouseButtonUp ( 0 ) && Physics.Raycast ( ray, out hit, 1000, interUnitLayer | WatcherLayer ) )
 		{
 			if ( 1 << hit.collider.gameObject.layer == interUnitLayer.value )
 			{
@@ -68,8 +69,11 @@ public class Player : MonoBehaviour
 			}
 		}
 		agent.SetDestination ( moveTarget );
+		Vector3 pos = glowIndicator.transform.position;
+		pos.y = iac!=null && !iac.isStronger (this.strongnes) ? 0.1f : -1;
+		glowIndicator.transform.position = pos;
 	}
-
+	
 	private void AskForHelp ( WatcherController ctrl, float strength )
 	{
 		GameObject inst;
@@ -77,7 +81,7 @@ public class Player : MonoBehaviour
 		{
 			karma += 15;
 			strongnes++;
-
+			
 			inst = (GameObject)Object.Instantiate ( SpeechbubblePrefab );
 			inst.transform.SetParent ( ctrl.transform, false );
 			Destroy ( inst, 5 );
@@ -88,19 +92,19 @@ public class Player : MonoBehaviour
 			inst.transform.SetParent ( ctrl.transform, false );
 			Destroy ( inst, 5 );
 		}
-
+		
 		inst = (GameObject)Object.Instantiate ( SpeechbubblePrefab );
 		inst.transform.SetParent ( transform, false );
 		Destroy ( inst, 5 );
 	}
-
+	
 	public void leaveArea ()
 	{
 		iac = null;
 		strongnes = 1;
-
+		
 	}
-
+	
 	public void CallHelp ()
 	{
 		var units = Physics.OverlapSphere ( transform.position, helpCallRange, interUnitLayer );
@@ -113,12 +117,12 @@ public class Player : MonoBehaviour
 			}
 		}
 	}
-
+	
 	public void callPolice ()
 	{
 		var pol = (GameObject)Object.Instantiate ( PolicePrefab );
 		pol.GetComponent<PoliceController> ().moveTo ( transform.position, this.iac );
 		world.Polices.Add ( pol );
 	}
-
+	
 }
