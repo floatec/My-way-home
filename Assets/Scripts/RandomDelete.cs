a5da309cfbfeef919bd48275ad0ec05a1af057e7
Benkei <Kutsarit@web.de>
2015-01-25 03:56:16 +0100

    update world scene

---
+++ Assets/Scripts/RandomDelete.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomDelete : MonoBehaviour
+{
+	public int Left = 1;
+
+	void Start ()
+	{
+		List<Transform> list = new List<Transform> ( transform.childCount );
+		foreach ( var item in transform )
+		{
+			list.Add ( (Transform)item );
+		}
+		while ( list.Count > Left )
+		{
+			var idx = Random.Range ( 0, list.Count );
+			Destroy ( list[idx].gameObject );
+			list.RemoveAt ( idx );
+		}
+	}
+}
